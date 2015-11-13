aae9a108b2cd260b76a899a1d0b2c62ca5c4829f
Steven Grissom <sggrissom@gmail.com>
2015-11-13 09:01:00 -0600

    started on problem 24. Working on permutations

--- problem.c
+++ problem.c
@@ -10,154 +10,64 @@
 #define LONG "%I64u"
 #endif
 
-typedef struct
-{
-    u32 *List;
-    u32 Length;
-} abundant_number_list;
-
-#define AbundantNumberLimit 28123
-global abundant_number_list AbundantNumberList;
-
 internal u32
-SumOfDivisors(u32 Number)
+Factorial(u32 X)
 {
-    u32 Sum = 0;
+    u32 Result = 1;
 
-    for(u32 PotentialDivisor = 1;
-        PotentialDivisor <= (u32)(Number/2);
-        ++PotentialDivisor)
-    {
-        if((Number % PotentialDivisor) == 0)
-        {
-            Sum += PotentialDivisor;
-        }
-    }
-
-    return Sum;
-}
-
-internal b32
-IsAbundant(u32 Number)
-{
-    return (SumOfDivisors(Number) > Number);
-}
-
-internal void
-PopulateAbundantNumberList()
-{
-    u32 *List = 0;
-    
-    for(u32 PotentialAbundantNumber = 1;
-        PotentialAbundantNumber < AbundantNumberLimit;
-        ++PotentialAbundantNumber)
-    {
-        if(IsAbundant(PotentialAbundantNumber))
-        {
-            buffer_push(List, PotentialAbundantNumber);
-        }
-    }
-
-    AbundantNumberList.List = List;
-    AbundantNumberList.Length = buffer_count(List);
-}
-
-internal b32
-IsInAbundantList(u32 Number)
-{
-    b32 Result = 0;
-    
-    for(u32 i = 0;
-        i < AbundantNumberList.Length;
+    for(u3 i = 2;
+        i <= X;
         ++i)
     {
-        if(AbundantNumberList.List[i] == Number)
-        {
-            Result = 1;
-            break;
-        }
+        Result *= X;
     }
 
     return Result;
 }
 
 internal u32
-AbundantListIndexOfLimit(u32 Number)
+GetPermutations(u32 PermutationNumber, u32 Size)
 {
     u32 Result = 0;
 
-    for(u32 i = 0;
-        i < AbundantNumberList.Length;
-        ++i)
-    {
-        if(AbundantNumberList.List[i] >= Number)
-        {
-            Result = i;
-            break;
-        }
-    }
-
-    return Result;
-}
-
-internal b32
-IsSumOfAbundantNumbers(u32 Number)
-{
-    u32 Limit = AbundantListIndexOfLimit(Number);
+    u32 *Numbers = (u32 *)malloc(Size * sizeof(u32));
 
     for(u32 i = 0;
-        i < Limit;
+        i < Size;
         ++i)
     {
-        for(u32 j = 0;
-            j < Limit;
-            ++j)
-        {
-            u32 Sum = AbundantNumberList.List[i] + AbundantNumberList.List[j];
-            if(Sum == Number)
-            {
-                return 1;
-            }
-        }
+        Numbers[i] = i;
     }
 
-    return 0;
-}
+    u32 PermutationCount = Factorial(Size);
+    u32 *Permutations = (u32 *)malloc(Size * sizeof(u32));
 
-internal u32
-SumOfAllNumbersNotMadeUpOfAbundantNumbers()
-{
-    u32 Sum = 0;
+    u32 index = 0;
 
-    for(u32 PossibleNumber = 1;
-        PossibleNumber < AbundantNumberLimit;
-        ++PossibleNumber)
+    for(u32 i = 0;
+        i < Size;
+        ++i)
     {
-        if(!IsSumOfAbundantNumbers(PossibleNumber))
-        {
-            Sum += PossibleNumber;
-            if(PossibleNumber > 22000)
-                printf("%d\n", PossibleNumber);
-        }
     }
     
-    return Sum;
+    return Result;
 }
 
 s32 main(s32 argc, char *argv[])
 {
-    char *x = "1", *y = "2";
+    char *cx = "5", *cy = "3";
     if (argc == 2)
     {
-        x = argv[1];
+        cx = argv[1];
     } else if (argc == 3) {
-        x = argv[1];
-        y = argv[2];
+        cx = argv[1];
+        cy = argv[2];
     }
 
-    PopulateAbundantNumberList();
+    u32 x = sg_atoi(cx);
+    u32 y = sg_atoi(cy);
 
-    printf("%d\n", SumOfAllNumbersNotMadeUpOfAbundantNumbers());
+    printf("%d\n", GetLexigraphicPermutation(x, y));
 
     return 0;
 }
